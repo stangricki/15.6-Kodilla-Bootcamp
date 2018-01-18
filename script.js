82613874b82cfb61185e87bdafa3567d452d6622
Filip Stangricki <stangricki@gmail.com>
2018-01-18 21:57:27 +0100

    script.js improved

--- script.js
+++ script.js
@@ -1,95 +1,99 @@
 class Stopwatch {
-    constructor(display) {
-        this.running = false;
-        this.display = display;
-        this.reset();
-        this.print(this.times);
+  constructor(container) {
+    this.container = container
+    this.running = false;
+    this.display = this.container.querySelector('.stopwatch');
+    this.reset();
+    this.print(this.times);
+    this.build(this.container)
+  }
+  reset() {
+    this.times = {
+      minutes: 0,
+      seconds: 0,
+      miliseconds: 0
+    };
+  }
+  print() {
+    this.display.innerText = this.format(this.times);
+  }
+  format(times) {
+    return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
+  }
+  start() {
+    if (!this.running) {
+      this.running = true;
+      this.watch = setInterval(() => this.step(), 10);
     }
-    reset() {
-        this.times = {
-            minutes: 0,
-            seconds: 0,
-            miliseconds: 0
-        };
+  }
+  step() {
+    if (!this.running) return;
+    this.calculate();
+    this.print();
+  }
+  calculate() {
+    this.times.miliseconds += 1;
+    if (this.times.miliseconds >= 100) {
+      this.times.seconds += 1;
+      this.times.miliseconds = 0;
     }
-    print() {
-        this.display.innerText = this.format(this.times);
+    if (this.times.seconds >= 60) {
+      this.times.minutes += 1;
+      this.times.seconds = 0;
     }
-    format(times) {
-        return `${pad0(times.minutes)}:${pad0(times.seconds)}:${pad0(Math.floor(times.miliseconds))}`;
-    }
-    start() {
-        if (!this.running) {
-            this.running = true;
-            this.watch = setInterval(() => this.step(), 10);
-        }
-    }
-    step() {
-        if (!this.running) return;
-        this.calculate();
-        this.print();
-    }
-    calculate() {
-        this.times.miliseconds += 1;
-        if (this.times.miliseconds >= 100) {
-            this.times.seconds += 1;
-            this.times.miliseconds = 0;
-        }
-        if (this.times.seconds >= 60) {
-            this.times.minutes += 1;
-            this.times.seconds = 0;
-        }
-    }
-    stop() {
-        this.running = false;
-        clearInterval(this.watch);
-    }
+  }
+  stop() {
+    this.running = false;
+    clearInterval(this.watch);
+  }
+
+  build(container) {
+
+    // NA POTEM: wrzucic to w obiekt piekniutki
+    // const setup = {
+    //     'start': () => {},
+    //     'stop': () =>
+    // }
+
+    // Object.keys(setup).forEach()
+
+    const startButton = container.getElementsByClassName('start');
+    startButton[0].addEventListener('click', () => this.start());
+
+    const stopButton = container.getElementsByClassName('stop');
+    stopButton[0].addEventListener('click', () => {
+      this.stop();
+    });
+
+    const resetButton = container.getElementsByClassName('reset');
+    resetButton[0].addEventListener('click', () => {
+      this.reset();
+      this.print();
+      this.stop();
+    });
+
+    const saveButton = container.getElementsByClassName('save');
+    saveButton[0].addEventListener('click', () => {
+      this.container.querySelector('.results').innerHTML += `<li>${stopwatch.display.textContent}</li>`;
+    }); 
+
+    // delete BUTTON
+
+    const deleteButton = this.container.getElementsByClassName('delete');
+    deleteButton[0].addEventListener('click', () => {
+      this.container.querySelector('.results').innerHTML = " ";
+    }); 
+  }
 }
 
 function pad0(value) {
-    let result = value.toString();
-    if (result.length < 2) {
-        result = '0' + result;
-    }
-    return result;
+  let result = value.toString();
+  if (result.length < 2) {
+    result = '0' + result;
+  }
+  return result;
 }
 
-const stopwatch = new Stopwatch(
-    document.querySelector('.stopwatch')
-)
-
-// start BUTTON
-
-const startButton = document.getElementById('start');
-startButton.addEventListener('click', () => stopwatch.start());
-
-// stop BUTTON
-
-const stopButton = document.getElementById('stop');
-stopButton.addEventListener('click', () => {
-    stopwatch.stop();
-
-});
-
-// reset BUTTON
-
-const resetButton = document.getElementById('reset');
-resetButton.addEventListener('click', () => {
-    stopwatch.reset();
-    stopwatch.print();
-    stopwatch.stop();
-});
-
-// save BUTTON
-
-const saveButton = document.getElementById('save');
-saveButton.addEventListener('click', () => {
-    document.querySelector('.results').innerHTML += `<li>${stopwatch.display.textContent}</li>`;
-}); // jak to dodac do class?
-
-// delete BUTTON
-
-const deleteButton = document.getElementById('delete');
-deleteButton.addEventListener('click', () => {
-    document.querySelector('.results').innerHTML = " ";
-}); // jak to dodac do class?
+['.container__stopwatch--first', '.container__stopwatch--second'].forEach((selector) => {
+    new Stopwatch(document.querySelector(selector))
+})
